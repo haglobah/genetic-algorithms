486c90b7d5c2c0531c944db18195e356f0561d2d
Beat Hagenlocher <bah@posteo.de>
2023-09-17 15:24:50 +0200

    Add chapter 2

--- genetic/lib/genetic.ex
+++ genetic/lib/genetic.ex
@@ -3,32 +3,33 @@ defmodule Genetic do
   Documentation for `Genetic`.
   """
 
-  def inittialize(genotype) do
-    for _ <- 1..100, do: genotype.()
+  def initialize(genotype, opts \\ []) do
+    population_size = Keyword.get(opts, :population_size, 100)
+    for _ <- 1..population_size, do: genotype.()
   end
 
-  def evaluate(population, fitness_function) do
+  def evaluate(population, fitness_function, _opts \\ []) do
     population
     |> Enum.sort_by(fitness_function, &>=/2)
   end
 
-  def select(population) do
+  def select(population, _opts \\ []) do
     population
     |> Enum.chunk_every(2)
     |> Enum.map(&List.to_tuple(&1))
   end
 
-  def crossover(population) do
+  def crossover(population, _opts \\ []) do
     population
     |> Enum.reduce([], fn {p, q}, acc ->
       cx_point = :rand.uniform(length(p))
-      {{h1, t1}, {h2, t2}} = {Enum.split(p1, cx_point), Enum.split(p2, cx_point)}
+      {{h1, t1}, {h2, t2}} = {Enum.split(p, cx_point), Enum.split(q, cx_point)}
       {c1, c2} = {h1 ++ t2, h2 ++ t1}
       [c1, c2 | acc]
     end)
   end
 
-  def mutation(population) do
+  def mutate(population, _opts \\ []) do
     population
     |> Enum.map(fn chromosome ->
         if :rand.uniform() < 0.05 do
@@ -39,24 +40,24 @@ defmodule Genetic do
     end)
   end
 
-  def run(fitness_function, genotype, max_fitness) do
-    population = initialize()
+  def run(fitness_function, genotype, max_fitness, opts \\ []) do
+    population = initialize(genotype)
     population
-    |> evolve(fitness_function, genotype, max_fitness)
+    |> evolve(fitness_function, max_fitness, opts)
   end
 
-  def evolve(population, fitness_function, genotype, max_fitness) do
-    population = evaluate(popultation, fitness_function)
+  def evolve(population, fitness_function, max_fitness, opts \\ []) do
+    population = evaluate(population, fitness_function, opts)
     best = hd(population)
     IO.write("\nCurrent Best: #{fitness_function.(best)}")
     if fitness_function.(best) == max_fitness do
       best
     else
       population
-      |> select()
-      |> crossover()
-      |> mutate()
-      |> evolve(fitness_function, genotype, max_fitness)
+      |> select(opts)
+      |> crossover(opts)
+      |> mutate(opts)
+      |> evolve(fitness_function, max_fitness, opts)
     end
   end
 end
